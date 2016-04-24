38f1ad74e7131d4a56318b41d6a7fb9ff035d563
Maximilian Lackaw <mlackaw@gmail.com>
2016-04-24 17:30:19 +0200

    Add less to css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
+var del = require('del');
 
 var $ = require('gulp-load-plugins')({lazy: true});
 
@@ -13,6 +14,42 @@ gulp.task('vet', function() {
 		.pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
 		.pipe($.jshint.reporter('fail'));
 });
+
+gulp.task('styles', ['clean-styles'], function() {
+	log('Complie less --> CSS');
+
+	return gulp
+		.src(config.less)
+		.pipe($.plumber())
+		.pipe($.less())
+		//.on('error', errorLogger)
+		.pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+		.pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function(done) {
+	var files = config.temp + '/**/*.css';
+	clean(files, done);
+});
+
+gulp.task('less-watcher', function() {
+	gulp.watch([config.less], ['styles']);
+});
+
+function errorLogger(error) {
+	log('*** Start of Error ***');
+	log(error);
+	log('*** End of Error ***');
+	this.emit('end');
+}
+
+function clean(path, done) {
+	log('Cleaning: ' + $.util.colors.blue(path));
+	del(path)
+		.then(function() {
+			done();
+		});
+}
 
 function log(msg) {
 	if (typeof(msg) === 'object') {
