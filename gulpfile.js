8f9a042cd38da5aa227bdeb01a8ac6f224de71aa
Maximilian Lackaw <mlackaw@gmail.com>
2016-04-25 19:06:17 +0200

    Add serve-dev task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
 var del = require('del');
+var port = process.env.PORT || config.defaultPort;
 
 var $ = require('gulp-load-plugins')({lazy: true});
+
 
 gulp.task('vet', function() {
 	return gulp
@@ -22,7 +24,6 @@ gulp.task('styles', ['clean-styles'], function() {
 		.src(config.less)
 		.pipe($.plumber())
 		.pipe($.less())
-		//.on('error', errorLogger)
 		.pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
 		.pipe(gulp.dest(config.temp));
 });
@@ -34,6 +35,56 @@ gulp.task('clean-styles', function(done) {
 
 gulp.task('less-watcher', function() {
 	gulp.watch([config.less], ['styles']);
+});
+
+gulp.task('wiredep', function() {
+	log('Wire up the bower css js and app js into the html');
+	var options = config.getWiredepDefaultOptions();
+	var wiredep = require('wiredep').stream;
+
+	return gulp
+		.src(config.index)
+		.pipe(wiredep(options))
+		.pipe($.inject(gulp.src(config.js)))
+		.pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep', 'styles'], function() {
+	log('Wire up the app css into the html, and call wiredep');
+
+	return gulp
+		.src(config.index)
+		.pipe($.inject(gulp.src(config.css)))
+		.pipe(gulp.dest(config.client));
+});
+
+gulp.task('serve-dev', ['inject'], function() {
+	var isDev = true;
+
+	var nodeOptions = {
+		script: config.nodeServer,
+		delayTime: 1,
+		env: {
+			'PORT': port,
+			'NODE_ENV': isDev ? 'dev' : 'build'
+		},
+		watch: [config.server]
+	};
+	
+	return $.nodemon(nodeOptions)
+		.on('restart', function(ev) {
+			log('*** nodemon restarted');
+			log('files changed on restart: \n');
+		})
+		.on('start', function() {
+			log('*** nodemon started');
+		})
+		.on('crash', function() {
+			log('*** nodemon crashed: script crashed for some reason');
+		})
+		.on('exit', function() {
+			log('*** nodemon exited cleanly');
+		});
 });
 
 function errorLogger(error) {
